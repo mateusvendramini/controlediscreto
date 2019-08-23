b943f17e89fd5cce518dca65595401dcc5e66bf3
mateusvendramini <mateus.vendramini@usp.br>
2019-08-22 22:04:27 -0300

    Inicializa repositorio

---
+++ InitialParameters.m
@@ -0,0 +1,14 @@
+J = 100000
+MFuel0 = 8000
+M0 = 15000 - MFuel0
+Power = 3000
+MaxFt = 44000
+MaxFl = 500
+RTSI = 3000
+g = 1.6
+x_dot0 = 0
+y_dot0 = -700
+theta_dot0 = 0
+x0 = 500
+y0 = 160000
+theta0 = 0
